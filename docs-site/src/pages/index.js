d2ff3e63ca7bf413c29b975f233e41aa8ba3864c
farfromrefuge <dev@akylas.fr>
2026-05-13 11:30:18 +0200

    chore: website

--- docs-site/src/pages/index.js
+++ docs-site/src/pages/index.js
@@ -173,9 +173,18 @@ function DonationBanner() {
                         <a
                             href="https://opencollective.com/oss-appscollective"
                             target="_blank"
-                            style="display: inline-block; padding: 12px 24px; background: #0d2e67; color: white; text-decoration: none; border-radius: 6px; font-weight: 600;display: flex;"
+                            style={{
+                                display: 'inline-block',
+                                padding: '12px 24px',
+                                background: '#0d2e67',
+                                color: 'white',
+                                textDecoration: 'none',
+                                borderRadius: '6px',
+                                fontWeight: 600,
+                                display: 'flex'
+                            }}
                         >
-                            <svg role="img" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg" fill="#b5d2f0" style="margin-right:10;">
+                            <svg role="img" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg" fill="#b5d2f0" style={{ marginRight: 10 + 'em' }}>
                                 <title>Open Collective</title>
                                 <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12c2.54 0 4.894-.79 6.834-2.135l-3.107-3.109a7.715 7.715 0 1 1 0-13.512l3.107-3.109A11.943 11.943 0 0 0 12 0zm9.865 5.166l-3.109 3.107A7.67 7.67 0 0 1 19.715 12a7.682 7.682 0 0 1-.959 3.727l3.109 3.107A11.943 11.943 0 0 0 24 12c0-2.54-.79-4.894-2.135-6.834z" />
                             </svg>
